332e34fc205c7d8011cfc5d3527f2263c1f82314
Paul Egan <paulegan@mail.com>
2015-04-02 18:16:09 +0100

    Transition with movement

--- src/scripts/itemdetail.js
+++ src/scripts/itemdetail.js
@@ -1,7 +1,34 @@
 'use strict';
 
 angular.module('example-app')
-  .controller('ItemDetailCtrl', function($scope, $state, $famous, $controller, AppConfig, ItemService) {
+  .controller('ItemDetailCtrl', function($scope, $rootScope, $state, $famous, $timeline, $controller, AppConfig, ItemService) {
+    var Easing = $famous['famous/transitions/Easing'];
+
+    // Image header takes up 40% of screen with meta content taking the other 60%
+    var headSize = [$rootScope.screenSize[0], Math.floor($rootScope.screenSize[1] * 0.4)];
+    var metaSize = [$rootScope.screenSize[0], $rootScope.screenSize[1] - headSize[1]];
+
+    if ($rootScope.selectedItem) {
+      // Transition from selectedItem position & size
+      var pos = $rootScope.selectedItem.position;
+      $scope.viewTranslate = $timeline([
+          [0, [pos[0], pos[1], 100], Easing.inQuad],
+          [1, [0, 0, 100]]
+      ]);
+      $scope.headSize = $timeline([
+          [0, $rootScope.selectedItem.size],
+          [1, headSize]
+      ]);
+      $scope.metaSize = $timeline([
+          [0, [$rootScope.selectedItem.size[0], 0]],
+          [1, metaSize]
+      ]);
+    } else {
+      // No transition - stick with fixed position & size
+      $scope.viewTranslate = function () {return [0, 0];};
+      $scope.headSize = function () {return headSize;};
+      $scope.metaSize = function () {return metaSize;};
+    }
 
     ItemService.getItem({'photo_id': $state.params.item}, function (item) {
       $scope.item = item;
--- src/scripts/itemlist.js
+++ src/scripts/itemlist.js
@@ -1,16 +1,34 @@
 'use strict';
 
 angular.module('example-app')
-  .controller('ItemListCtrl', function($scope, $state, $famous, $controller, AppConfig, ItemService) {
+  .controller('ItemListCtrl', function($scope, $rootScope, $state, $famous, $controller, AppConfig, ItemService) {
     var query = $state.params.q || 'example';
+    var scrollViewNode = null;
 
+    $scope.headerSize = 50;
     $scope.title = 'Items matching "' + query + '"';
 
     ItemService.getList({text: query}, function (items) {
       $scope.items = items;
+      scrollViewNode = $famous.find('#itemScrollView')[0].renderNode;
+      if ($rootScope.selectedItem) {
+        scrollViewNode.setPosition($rootScope.selectedItem.scrollPosition);
+      }
     });
 
     $scope.itemClickHandler = function (event, itemId) {
+      var surface = $famous.find('#item' + itemId)[0];
+      var index = surface.index || 0;
+      var scrollOffset = $scope.headerSize - scrollViewNode.getAbsolutePosition();
+      var size = surface.renderNode.getSize();
+      var position = [0, scrollOffset + index * size[1]];
+      $rootScope.selectedItem = {
+        id: itemId,
+        index: index,
+        size: size,
+        position: position,
+        scrollPosition: scrollViewNode.getAbsolutePosition()
+      };
       $state.go('itemdetail', {item: itemId});
     };
 
